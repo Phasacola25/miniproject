078f56d438ff1e023fd75560594988c342cc47cd
Phasacola <h1101251037@student.untan.ac.id>
2026-09-23 20:15:44 +0700

    update kode motor

--- motor.php
+++ motor.php
@@ -1,27 +1,29 @@
 <?php
 
-class Motor extends Kendaraan
-{
+class Motor extends Kendaraan{
+
     protected $jenisMotor;
 
-    public function __construct($merk, $nomorPlat, $hargaSewa, $jenisMotor)
+    public function __construct($nama, $merk, $nomorPlat, $hargaSewa, $jenisMotor)
     {
-        parent::__construct($merk, $nomorPlat, $hargaSewa);
+        parent::__construct($nama, $merk, $nomorPlat, $hargaSewa);
 
+        if ($jenisMotor === ""){
+            throw new InvalidArgumentException("Jenis Motor Tidak Boleh Kosong");
+        }
         $this->jenisMotor = $jenisMotor;
     }
 
-    public function hitungBiayaSewa($hari)
-    {
-        return $this->hargaSewa * $hari;
+    public function hitungBiayaSewa($hari){
+        if (!is_numeric($hari) || $hari <= 0){
+            throw new InvalidArgumentException("Lama sewa harus lebih dari 0 hari");
+        }
+        return $this->gethargaSewa() * $hari;
     }
 
-    public function getInfo()
-    {
-        return "Motor " . $this->merk .
-               ", Plat: " . $this->nomorPlat .
-               ", Harga Sewa: Rp " . $this->hargaSewa .
-               ", Jenis: " . $this->jenisMotor;
+    public function getInfo(){
+
+        return parents::getInfo(). "Jenis Motor : ". $this->jenisMotor;
     }
 }
 
