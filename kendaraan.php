ebe05084ad2b68eaf968fd626c6cb515806c3bcf
Phasacola <h1101251037@student.untan.ac.id>
2026-09-23 19:43:18 +0700

    update kode kendaraan

--- kendaraan.php
+++ kendaraan.php
@@ -1,13 +1,17 @@
 <?php
 
-abstract class Kendaraan
-{
+abstract class Kendaraan {
+    protected $nama;
     protected $merk;
     protected $nomorPlat;
     protected $hargaSewa;
+    protected $ketersediaan;
 
-    public function __construct($merk, $nomorPlat, $hargaSewa)
-    {
+    public function __construct($nama,$merk, $nomorPlat, $hargaSewa) {
+        if (!is_numeric($hargaSewa)||$hargaSewa <= 0) {
+            throw new InvalidArgumentException("Harga sewa harus lebih besar dari 0.");}
+
+        $this->nama = $nama;
         $this->merk = $merk;
         $this->nomorPlat = $nomorPlat;
         $this->hargaSewa = $hargaSewa;
@@ -15,11 +19,26 @@ abstract class Kendaraan
 
     abstract public function hitungBiayaSewa($hari);
 
-    public function getInfo()
-    {
-        return "Merk: " . $this->merk .
-               ", Nomor Plat: " . $this->nomorPlat .
-               ", Harga Sewa: Rp " . $this->hargaSewa;
+    public function getNama(){
+        return $this->nama;
+    }
+
+    public function getHargaSewa() {
+        return $this->hargaSewa;
+    }
+    
+    public function ketersediaan() {
+        return $this->ketersediaan;
+    }
+
+    public function setKetersediaan($ketersediaan){
+        $this->ketersediaan = (bool) $ketersediaan;
+    }
+
+    public function getInfo(){
+        $status = $this->ketersediaan ? "Tersedia" : "Tidak Tersedia";
+      
+        return "Nama Kendaraan : " . $this->nama . ",Merk: " . $this->merk .", Nomor Plat: " . $this->nomorPlat .", Harga Sewa: Rp " . number_format($this->hargaSewa, 0, ',', '.'). ", Status : ". $status;
     }
 }
 
