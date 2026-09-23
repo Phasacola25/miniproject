8ba93ec6dc646e9e903e5cc91c33c171d6c309e6
Phasacola <h1101251037@student.untan.ac.id>
2026-09-23 19:57:49 +0700

    update kode mobil

--- mobil.php
+++ mobil.php
@@ -4,24 +4,25 @@ class Mobil extends Kendaraan
 {
     protected $jumlahKursi;
 
-    public function __construct($merk, $nomorPlat, $hargaSewa, $jumlahKursi)
-    {
-        parent::__construct($merk, $nomorPlat, $hargaSewa);
+    public function __construct($nama,$merk, $nomorPlat, $hargaSewa, $jumlahKursi){
+        parent::__construct($nama, $merk, $nomorPlat, $hargaSewa);
 
-        $this->jumlahKursi = $jumlahKursi;
+        
+        if (!is_numeric($jumlahKursi) || $jumlahKursi <= 0){
+            throw new InvalidArgumentException("Jumlah Kursi Tidak Valid");
+        } $this->jumlahKursi = $jumlahKursi;
     }
 
-    public function hitungBiayaSewa($hari)
-    {
-        return $this->hargaSewa * $hari;
+    public function hitungBiayaSewa($hari){
+        if (!is_numeric($hari) || $hari <= 0){
+            throw new InvalidArgumentException(" Lama sewa harus lebih dari 0 hari");
+        }
+        return $this->getHargaSewa() *$hari;
+    
     }
 
-    public function getInfo()
-    {
-        return "Mobil " . $this->merk .
-               ", Plat: " . $this->nomorPlat .
-               ", Harga Sewa: Rp " . $this->hargaSewa .
-               ", Jumlah Kursi: " . $this->jumlahKursi;
+    public function getInfo(){
+        return parent::getInfo() . "Jumlah Kursi : " . $this->jumlahKursi;
     }
 }
 ?>
